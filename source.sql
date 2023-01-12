2f6357b86ce5b8886b781fd079e1ea73b53eb6e2
bilrazor <113417155+bilrazor@users.noreply.github.com>
2023-01-12 11:06:24 +0100

    source.sql (Corrección)

--- source.sql
+++ source.sql
@@ -3,13 +3,14 @@ CREATE DATABASE RBD_GYM;
 USE RBD_GYM;
 
 CREATE TABLE tPersona (
-DNI INTEGER PRIMARY KEY AUTO_INCREMENT,
+DNI varchar(20) PRIMARY KEY,
 Nombre VARCHAR(50),
 Correo VARCHAR(200),
 Pago Integer,
 Dirección varchar(200),
 Nºteléfono Integer
 );
+
 
 create table tPedidos(
     Id integer primary key auto_increment,
@@ -41,11 +42,11 @@ create table Categoria(
 );
 
 
-INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES (54701531Q, 'John Smith', 'johnsmith@example.com', 30, '123 Main St', 5555551);
-INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES (54701532Q, 'Jane Doe', 'janedoe@example.com', 30, '456 Park Ave',5555552);
-INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES (54701533Q, 'Bob Johnson', 'bobjohnson@example.com', 30, '789 Elm St', 5555553);
-INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES (54701534Q, 'Amy Miller', 'amymiller@example.com', 28, '321 Oak St', 5555554);
-INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES (54701535Q, 'Mark Wilson', 'markwilson@example.com', 28,'654 Pine St', 5555555);
+INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES ("54701531Q", 'John Smith', 'johnsmith@example.com', 30, '123 Main St', 5555551);
+INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES ("54701532Q", 'Jane Doe', 'janedoe@example.com', 30, '456 Park Ave',5555552);
+INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES ("54701533Q", 'Bob Johnson', 'bobjohnson@example.com', 30, '789 Elm St', 5555553);
+INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES ("54701534Q", 'Amy Miller', 'amymiller@example.com', 28, '321 Oak St', 5555554);
+INSERT INTO tPersona (DNI, Nombre, Correo, Pago, Dirección, Nºteléfono) VALUES ("54701535Q", 'Mark Wilson', 'markwilson@example.com', 28,'654 Pine St', 5555555);
 
  
 insert into tPedidos(Fecha, Cantidad) VALUES ("2022-11-30", "4");
@@ -64,9 +65,9 @@ insert into tClases(Nombre, Horarios) VALUES ("Zumba", "21:00");
 
 
 
-insert into Categorias('Nombre', 'Imagen') values ('Ropa','https://www.todo-yoga.net/wp-content/uploads/2018/07/camiseta-blanca-unisex-algoddon-416x416.jpg')
-insert into Categorias('Nombre', 'Imagen') values ('Shaker','https://www.gastrolabweb.com/u/fotografias/fotosnoticias/2021/4/7/11334.jpg')
-insert into Categorias('Nombre', 'Imagen') values ('Suplementos','https://chemaarguedas.com/cms/wp-content/uploads/2019/11/Captura-de-pantalla-2020-05-20-a-las-12.29.55.jpg')
+insert into Categorias('Nombre', 'Imagen') values ('Ropa','https://www.todo-yoga.net/wp-content/uploads/2018/07/camiseta-blanca-unisex-algoddon-416x416.jpg');
+insert into Categorias('Nombre', 'Imagen') values ('Shaker','https://www.gastrolabweb.com/u/fotografias/fotosnoticias/2021/4/7/11334.jpg');
+insert into Categorias('Nombre', 'Imagen') values ('Suplementos','https://chemaarguedas.com/cms/wp-content/uploads/2019/11/Captura-de-pantalla-2020-05-20-a-las-12.29.55.jpg');
 
 
 
